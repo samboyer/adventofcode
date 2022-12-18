9055f06a43cdf07188a5741906831f99ee4f9979
Sam Boyer <samboyer@users.noreply.github.com>
2022-12-18 13:26:48 +0000

    various savings to day 7 (-12 chars)

--- 2022/day7.min.py
+++ 2022/day7.min.py
@@ -1,7 +1,6 @@
-def f(c):
- global b,t
- if c[1]=='cd':
-  if c[2][0]=='.':t+=x if(x:=b.pop())<=100000 else 0;b[-1]+=x
-  else:b+=[0]
- b[-1]+=int(c[0])if c[0].isdigit()else 0
-b=[];t=0;[f(l.split(' '))for l in open('i/7').readlines()];[f(['','cd','.'])for _ in b];print(t)
+def f(s,c,a=0):
+ if c=='cd':
+  if a[0]<'/':b[0]+=x if(x:=b.pop())<=100000 else 0;b[-1]+=x
+  else:b.append(0)
+ if s.isdigit():b[-1]+=int(s)
+b=[0];[f(*l.split(' '))for l in open('i/7').readlines()];[f('','cd','.')for _ in b];print(b[0])
